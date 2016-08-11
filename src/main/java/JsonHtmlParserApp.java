bf3837402110ec5d6a982278529bbecc0e88d946
Romansyrotenko <roman.sirotenko@gmail.com>
2016-08-12 00:24:06 +0300

    first commit

---
+++ src/main/java/JsonHtmlParserApp.java
@@ -0,0 +1,64 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.safety.Whitelist;
+import org.jsoup.select.Elements;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class JsonHtmlParserApp {
+
+    static String BASE_URL = "http://www.springfieldspringfield.co.uk/";
+    static String PAGE_URL_WITH_ALL_EPISODES = "episode_scripts.php?tv-show=the-simpsons";
+    static String EPISODE_CONTAINER = ".scrolling-script-container";
+    static String EPISODE_NAME = ".main-content-left>h3";
+    static String LIST_OF_EPISODES = ".season-episode-title";
+
+    public static void main(String args[]) throws IOException {
+
+        Document doc = Jsoup.connect(BASE_URL + PAGE_URL_WITH_ALL_EPISODES).get();
+        Elements allEpisodesUrls = doc.select(LIST_OF_EPISODES);
+
+        for (int i = 0; i < allEpisodesUrls.size(); i++) {
+            parsePageToFile(getCurrentEpisodeUrl(allEpisodesUrls.get(i)));
+        }
+    }
+
+    private static String getCurrentEpisodeUrl(Element element) {
+        return BASE_URL + element.attr("href");
+    }
+
+    private static void parsePageToFile(String url) throws IOException {
+
+        Document episode = Jsoup.connect(url).get();
+        String currentEpisodeName = episode.select(EPISODE_NAME).get(0).text().substring(6);
+        Elements episodeScript = episode.select(EPISODE_CONTAINER);
+        String episodePlainText = Jsoup.clean(episodeScript.toString(), Whitelist.basic());
+        String currentEpisodeNumber = url.substring(url.length() - 6);
+
+        writeStringToFile(currentEpisodeNumber, currentEpisodeName, episodePlainText);
+
+    }
+
+    private static void writeStringToFile(String episodeNumber, String episodeName, String plainText) throws IOException {
+
+        BufferedWriter outputFile = null;
+
+        try {
+            File file = new File(episodeNumber + " - " + episodeName + ".txt");
+            outputFile = new BufferedWriter(new FileWriter(file));
+            outputFile.write(plainText);
+        } catch ( IOException e ) {
+            e.printStackTrace();
+        } finally {
+            if ( outputFile != null ) {
+                outputFile.close();
+                System.out.println("File " + episodeNumber + " - " + episodeName + ".txt was successfully saved");
+            }
+        }
+    }
+
+}
